2784b86bcbef93ab9211701abdabe8fe50cf6da3
Alen Sendro <sendro.alen@nsoft.com>
2022-06-13 12:44:13 +0200

    Install moncao editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
-
+import { NuMonacoEditorDiffModel, NuMonacoEditorEvent, NuMonacoEditorModel } from '@ng-util/monaco-editor';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'my-app';
+  disabled = true;
+  themes = ['vs', 'vs-dark', 'hc-black'];
+  options = { theme: 'vs' };
+  
+  model: NuMonacoEditorModel = {
+    value: '<h1>Title</h1>',
+    language: 'html',
+  };
+  
+  oldModel: NuMonacoEditorDiffModel = {
+    code: 'const a = 1;',
+    language: 'typescript',
+  };
+  
+  newModel: NuMonacoEditorDiffModel = {
+    code: `
+      const a = 2;
+      console.log('test');
+      const c = 3;
+    `,
+    language: 'typescript',
+  };
+  
+  setTheme(theme: string): void {
+    this.options = { theme };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,56 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NuMonacoEditorModule } from '@ng-util/monaco-editor';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // MonacoEditorModule.forRoot({
+    //   // dynamicImport: () => import('monaco-editor'),
+    //   baseUrl: 'lib/v1',
+    //   defaultOptions: {},
+    // }),
+    NuMonacoEditorModule.forRoot({
+      defaultOptions: { scrollBeyondLastLine: false },
+      monacoLoad: (m) => {
+        const uri = m.Uri.parse('a://b/foo.json');
+        m.languages.json.jsonDefaults.setDiagnosticsOptions({
+          validate: true,
+          schemas: [
+            {
+              uri: 'http://myserver/foo-schema.json',
+              fileMatch: [uri.toString()],
+              schema: {
+                type: 'object',
+                properties: {
+                  p1: {
+                    enum: ['v1', 'v2'],
+                  },
+                  p2: {
+                    $ref: 'http://myserver/bar-schema.json',
+                  },
+                },
+              },
+            },
+            {
+              uri: 'http://myserver/bar-schema.json',
+              fileMatch: [uri.toString()],
+              schema: {
+                type: 'object',
+                properties: {
+                  q1: {
+                    enum: ['x1', 'x2'],
+                  },
+                },
+              },
+            },
+          ],
+        });
+      },
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
